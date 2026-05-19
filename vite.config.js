3d59a65647e04588447f8c3ed366aebff8ed367e
Johan Vaatstra <johan.vaatstra@outlook.com>
2026-05-19 10:27:09 +0200

    fix PWA cache via service worker unregister en vite hash

--- vite.config.js
+++ vite.config.js
@@ -8,5 +8,14 @@ export default defineConfig({
   server: {
     host: true,
     port: 5174
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]'
+      }
+    }
   }
 })
